d2bb4c373d60ba30bd59bd4a57824824126c4fec
Borloo <supermaxime753@gmail.com>
2022-11-30 14:59:14 +0100

    Finish init complet

--- pizzamania/src/app/app-routing.module.ts
+++ pizzamania/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AccueilComponent} from "./accueil/accueil.component";
+import {CommandeComponent} from "./commande/commande.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AccueilComponent},
+  {path: 'commande', component: CommandeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
